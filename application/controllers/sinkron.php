c01a7335f66ec5d6cda824a3f0d22bed249df303
muzakirnur <muzakir.nurr@gmail.com>
2022-05-17 18:11:11 +0700

    Fixing

--- application/controllers/sinkron.php
+++ application/controllers/sinkron.php
@@ -15,9 +15,36 @@ class sinkron extends CI_Controller
     public function pemanggil()
     {
         $data['data'] = $this->M_data->get_data('absensi')->result();
-        var_dump($data);
-        $this->load->view('admink/v_header');
-        $this->load->view('admink/dump');
-        $this->load->view('admink/v_footer');
+        foreach ($data as $d) {
+            $absensi_id = $d->absensi_id;
+            $nama = $d->nama;
+            $id_kelas = $d->id_kelas;
+            $nama_ortu = $d->nama_ortu;
+            $ket_pengajian = $d->ket_pengajian;
+            $hari = $d->hari;
+            $jam_pengajian = $d->jam_pengajian;
+            $tanggal_mulai = $d->tanggal_mulai;
+            $absen = $d->absen;
+            $kelas = $d->kelas;
+            $gampong = $d->gampong;
+            $kecamatan = $d->kecamatan;
+
+            $ins = array(
+                'absensi_id' => $absensi_id,
+                'nama' => $nama,
+                'id_kelas' => $id_kelas,
+                'nama_ortu' => $nama_ortu,
+                'ket_pengajian' => $ket_pengajian,
+                'hari' => $hari,
+                'jam_pengajian' => $jam_pengajian,
+                'tanggal_mulai' => $tanggal_mulai,
+                'absen' => $absen,
+                'kelas' => $kelas,
+                'gampong' => $gampong,
+                'kecamatan' => $kecamatan
+            );
+
+            $this->m_data->insert_data($ins, 'absensi');
+        }
     }
 }
